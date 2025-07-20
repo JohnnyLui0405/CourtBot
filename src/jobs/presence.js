efef7694bf78f2bc778c3c8812ad2646e95965b1
Johnny Lui <johnnylui2017@gmail.com>
2025-07-20 18:40:51 +0800

    refactor: presence rotation variable names
    Renamed variables 'count' and 'countTotal' to 'presenceIndex' and 'maxPresenceIndex' for improved clarity in the presence rotation logic. Updated all related references to use the new variable names.

--- src/jobs/presence.js
+++ src/jobs/presence.js
@@ -14,28 +14,26 @@ const getWeatherWarnings = async () => {
     return weatherData.data;
 };
 
-let count = 0;
-let countTotal = 2;
+let presenceIndex = 0;
+let maxPresenceIndex = 2;
 let weatherData = null;
 export const action = async (client) => {
     return new CronJob("*/10 * * * * *", async () => {
         logger.info(`Running ${job.name} job...`);
 
-        if (count === 0) {
+        if (presenceIndex === 0) {
             weatherData = await getWeatherWarnings();
 
-            countTotal = 2;
-            countTotal += weatherData.warningMessage.length;
-            logger.info(`Count total: ${countTotal}`);
+            maxPresenceIndex = 2;
+            maxPresenceIndex += weatherData.warningMessage.length;
+            logger.info(`Count total: ${maxPresenceIndex}`);
 
             const currentTime = `${new Date().toLocaleTimeString("zh-HK", { timeZone: "Asia/Hong_Kong" })} (GMT+8)`;
             logger.info(`Current time: ${currentTime}`);
 
             await client.user.setActivity(currentTime, { type: ActivityType.Listening });
-        } else if (count === 1) {
-            const uvIndex = (weatherData.uvindex && Array.isArray(weatherData.uvindex.data) && weatherData.uvindex.data.length > 0)
-                ? weatherData.uvindex.data[0].value
-                : "N/A";
+        } else if (presenceIndex === 1) {
+            const uvIndex = weatherData.uvindex && Array.isArray(weatherData.uvindex.data) && weatherData.uvindex.data.length > 0 ? weatherData.uvindex.data[0].value : "N/A";
             const temperature = weatherData.temperature.data[0].value;
             const humidity = weatherData.humidity.data[0].value;
             if (uvIndex === "N/A") {
@@ -44,12 +42,12 @@ export const action = async (client) => {
             await client.user.setActivity(`溫度: ${temperature}°C | 濕度: ${humidity}% | UV: ${uvIndex}`, { type: ActivityType.Listening });
             logger.info(`UV Index: ${uvIndex} | Temperature: ${temperature}°C | Humidity: ${humidity}%`);
         } else {
-            const weatherWarning = weatherData.warningMessage[count - 2];
+            const weatherWarning = weatherData.warningMessage[presenceIndex - 2];
             await client.user.setActivity(weatherWarning, { type: ActivityType.Listening });
             logger.info(`Weather warning: ${weatherWarning}`);
         }
 
-        count = (count + 1) % countTotal;
+        presenceIndex = (presenceIndex + 1) % maxPresenceIndex;
 
         logger.info(`${job.name} job Done!`);
     });
